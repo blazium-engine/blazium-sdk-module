3424ad3b6fefde98e7b60f5077d26bd1a066c3a0
Dragos Daian <daian.dragos@yahoo.com>
2025-01-15 10:19:49 +0100

    re-add

--- discord/discord_embedded_app_client.h
+++ discord/discord_embedded_app_client.h
@@ -32,6 +32,7 @@
 #define DISCORD_EMBEDDED_APP_CLIENT_H
 
 #include "../third_party_client.h"
+#include "platform/web/api/javascript_bridge_singleton.h"
 #include "scene/main/node.h"
 #include "discord_embedded_app_response.h"
 
