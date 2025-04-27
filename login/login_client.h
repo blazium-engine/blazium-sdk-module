3aed44dc79518bcb2915d33eddfdc8a974a3a9cc
Dragos Daian <daian.dragos@yahoo.com>
2025-04-27 12:49:09 +0200

    update urls to api/v1

--- login/login_client.h
+++ login/login_client.h
@@ -46,10 +46,10 @@ protected:
 	String websocket_prefix = "wss://";
 	String http_prefix = "https://";
 	String game_id = "";
-	String connect_route = "/connect";
-	String access_code_route = "/auth";
-	String verify_jwt_route = "/token/verify";
-	String refresh_jwt_route = "/token/refresh";
+	String connect_route = "/api/v1/connect";
+	String access_code_route = "/api/v1/auth";
+	String verify_jwt_route = "/api/v1/token/verify";
+	String refresh_jwt_route = "/api/v1/token/refresh";
 	bool connected = false;
 
 public:
